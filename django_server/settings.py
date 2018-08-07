62348b9087d9055edeb3bb19bb62bfa49560a967
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-08-07 16:31:39 -0400

    Recombined settings files using environment variables with fallbacks.

--- django_server/settings.py
+++ django_server/settings.py
@@ -1,13 +1,16 @@
 import os
 
 
-# import dev or prod settings
+# set debug and hosts
 if os.getenv('DJANGO_ENV') == 'prod':
-    from .settings_prod import *
+    DEBUG = False
+    ALLOWED_HOSTS = ['.aswwu.com']
 else:
-    from .settings_dev import *
+    DEBUG = True
+    ALLOWED_HOSTS = []
 
 # base
+SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_URLCONF = 'django_server.urls'
 WSGI_APPLICATION = 'django_server.wsgi.application'
@@ -19,6 +22,50 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+# databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'default',
+        'USER': os.getenv('MYSQL_USER', 'root'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'root'),
+        'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.getenv('MYSQL_PORT', 8889),
+    },
+    'elections': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elections',
+        'USER': os.getenv('MYSQL_USER', 'root'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'root'),
+        'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.getenv('MYSQL_PORT', 8889),
+    },
+    'jobs': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jobs',
+        'USER': os.getenv('MYSQL_USER', 'root'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'root'),
+        'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.getenv('MYSQL_PORT', 8889),
+    },
+    'pages': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pages',
+        'USER': os.getenv('MYSQL_USER', 'root'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'root'),
+        'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.getenv('MYSQL_PORT', 8889),
+    },
+    'people': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'people',
+        'USER': os.getenv('MYSQL_USER', 'root'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'root'),
+        'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.getenv('MYSQL_PORT', 8889),
+    },
+}
 
 # django setup
 INSTALLED_APPS = [
