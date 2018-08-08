952dd0c9ab16d613c38401c9dc8811d18365a780
Sheldon Woodward <sheldon.woodward@ahss.org>
2018-08-08 10:14:23 -0400

    Added test for 'ping' endpoint.

--- apps/ping/tests.py
+++ apps/ping/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestCalls(TestCase):
+    def test_ping_get(self):
+        response = self.client.get('/ping', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), '{"result":"pong!"}')
